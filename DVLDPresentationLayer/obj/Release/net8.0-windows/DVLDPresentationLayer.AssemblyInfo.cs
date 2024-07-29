b5ed9f4204de69186d1aebac3458697d4fd36f54
Laith <laithmallooh@gmail.com>
2024-07-29 21:47:43 +0300

    fixed manage people form

--- DVLDPresentationLayer/obj/Release/net8.0-windows/DVLDPresentationLayer.AssemblyInfo.cs
+++ DVLDPresentationLayer/obj/Release/net8.0-windows/DVLDPresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DVLDPresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26956d1b0e2fdcc100bada2ac9439d1664ffacaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2bfdb9187dcd72e15a7b8459d36eada686578b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DVLDPresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DVLDPresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
